6bfa68bf944a83ebf76f34b17c277d73a1e54855
Anthony Dray Pilo <anthonydraypilo@MBP-de-Anthony.lan>
2020-05-16 18:42:46 +0300

    linked to DB

--- hackathon2.py
+++ hackathon2.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired, Email, Length
@@ -6,18 +7,33 @@ from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "thisissecretpassword"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:Mamita2019@localhost:5432/hackathon"
 bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = "user"
+    user_id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(20))
+    lastname = db.Column(db.String(20))
+    username = db.Column(db.String(20), unique=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(100))
+
 
 
 class Login(FlaskForm):
-    username = StringField('username', validators=[DataRequired(), Length(min=4, max=20)])
-    password = PasswordField('password', validators=[DataRequired(), Length(min=8, max=100)])
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=20)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=100)])
 
 
 class Signin(FlaskForm):
-    email = StringField('email', validators=[DataRequired(), Length(max=50)])
-    username = StringField('username', validators=[DataRequired(), Length(min=4, max=20)])
-    password = PasswordField('password', validators=[DataRequired(), Length(min=8, max=100)])
+    firstname = StringField('Firstame', validators=[DataRequired(), Length(min=3, max=20)])
+    lastname = StringField('Lastname', validators=[DataRequired(), Length(min=3, max=20)])
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Length(max=50)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=100)])
 
 
 @app.route("/")
@@ -28,12 +44,22 @@ def index():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = Login()
+    if form.validate_on_submit():
+        return form.username.data
+
     return render_template("login.html", form=form)
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = Signin()
+    if form.validate_on_submit():
+        new_user = User(firstname=form.firstname.data, lastname=form.lastname.data, username=form.username.data,
+                        email=form.email.data, password=form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+        return form.username.data
+
     return render_template("signup.html", form=form)
 
 
