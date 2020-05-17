5063d9980640187806123408886526e60e2af868
Anthony Dray Pilo <anthonydraypilo@MBP-de-Anthony.lan>
2020-05-17 17:08:00 +0300

    actualized

--- hackathon2.py
+++ hackathon2.py
@@ -1,17 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify, make_response, json
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired, Email, Length
 from flask_bootstrap import Bootstrap
-
+from flask_cors import CORS
+from pusher import pusher
+import simplejson
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "thisissecretpassword"
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:Mamita2019@localhost:5432/hackathon"
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
+# configure pusher object
+pusher = pusher.Pusher(
+app_id="1002493",
+key="792745e0907fdb507199",
+secret="c9c5693de1185d9d3164",
+cluster="ap2",
+ssl=True)
 
 class User(db.Model):
     __tablename__ = "user"
@@ -19,8 +30,12 @@ class User(db.Model):
     firstname = db.Column(db.String(20))
     lastname = db.Column(db.String(20))
     username = db.Column(db.String(20), unique=True)
+    adress = db.Column(db.String())
+    city = db.Column(db.String())
+    country = db.Column(db.String())
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(100))
+
 
 
 
@@ -33,6 +48,9 @@ class Signin(FlaskForm):
     firstname = StringField('Firstame', validators=[DataRequired(), Length(min=3, max=20)])
     lastname = StringField('Lastname', validators=[DataRequired(), Length(min=3, max=20)])
     username = StringField('Username', validators=[DataRequired(), Length(min=3, max=20)])
+    adress = StringField('Adress', validators=[DataRequired()])
+    city= StringField('City', validators=[DataRequired()])
+    country = StringField('Country', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Length(max=50)])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=100)])
 
@@ -40,6 +58,27 @@ class Signin(FlaskForm):
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+
+@app.route('/new/guest', methods=['POST'])
+def guestUser():
+    data = request.json
+    pusher.trigger(u'general-channel', u'new-guest-details', {
+        'name' : data['name'],
+        'email' : data['email']
+        })
+    return json.dumps(data)
+
+
+@app.route("/pusher/auth", methods=['POST'])
+def pusher_authentication():
+    auth = pusher.authenticate(channel=request.form['channel_name'],socket_id=request.form['socket_id'])
+    return json.dumps(auth)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -59,7 +98,7 @@ def login():
 def signup():
     form = Signin()
     if form.validate_on_submit():
-        new_user = User(firstname=form.firstname.data, lastname=form.lastname.data, username=form.username.data,
+        new_user = User(firstname=form.firstname.data, lastname=form.lastname.data, username=form.username.data, adress=form.adress.data, city=form.city.data, country=form.country.data,
                         email=form.email.data, password=form.password.data)
         db.session.add(new_user)
         db.session.commit()
